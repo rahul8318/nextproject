bb89789a85985790858395e673b331baade1f275
Rahul Kumar <krahul89kumar@gmail.com>
2025-10-19 01:16:42 +0530

    Secound commit

---
+++ models/User.ts
@@ -0,0 +1,34 @@
+import mongoose,{Schema, model, models} from "mongoose";
+import bcrypt from "bcryptjs";
+
+
+export interface Iuser {
+  email: string;
+  password: string;
+  _id?: mongoose.Types.ObjectId;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+const userSchema = new Schema<Iuser>(
+  {
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+
+  },
+  { timestamps: true}
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+const User = models.User || model<Iuser>("User", userSchema);
+
+export default User;
